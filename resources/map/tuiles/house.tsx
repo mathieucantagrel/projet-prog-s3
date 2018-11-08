85d3607886945613aa630320c447d0d02bf424b7
TheGiu99 <37263855+TheGiu99@users.noreply.github.com>
2018-11-08 19:47:31 +0100

    Add files via upload

---
+++ resources/map/tuiles/house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="house" tilewidth="32" tileheight="32" tilecount="63" columns="9">
+ <image source="house.png" trans="ff00ff" width="288" height="224"/>
+</tileset>
